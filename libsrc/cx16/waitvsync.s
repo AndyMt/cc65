108a0c1db94e736d1337af0198d43285b3daee25
AndyMt <github@mettauer.net>
2021-04-06 11:12:06 +0200

    Update waitvsync.s
    using RDTIM for the moment.

--- libsrc/cx16/waitvsync.s
+++ libsrc/cx16/waitvsync.s
@@ -9,13 +9,23 @@
 
         .export         _waitvsync
 
+        .import         RDTIM
+        .importzp       sreg
+
         .include        "cx16.inc"
 
 _waitvsync:
         ldx     BANK_RAM_SEL    ; (TIMER is in RAM bank 0)
         stz     BANK_RAM_SEL
-        lda     TIMER + 2
-:       cmp     TIMER + 2
-        beq     :-              ; Wait for next jiffy
+        
+        jsr     RDTIM
+        sta     sreg
+:       jsr     RDTIM
+        cmp     sreg 
+        beq     :-       
+;ToDo: Replace the above as soon as r39 ROM is stable
+;        lda     TIMER + 2
+;:       cmp     TIMER + 2
+;        beq     :-              ; Wait for next jiffy
         stx     BANK_RAM_SEL
         rts
